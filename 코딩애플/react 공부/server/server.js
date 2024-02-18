2f4ab8a024cacde7ea001ad0d770a9622cbe7ce1
안지원 <wl990@naver.com>
2024-02-18 18:39:58 +0900

    24.02.18 코딩애플 리액트

---
+++ 코딩애플/react 공부/server/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.listen(8080, function(){
+    console.log('listening on 8080')
+})
--- 코딩애플/react 공부/shop/src/routes/Testing.js
+++ 코딩애플/react 공부/shop/src/routes/Testing.js
@@ -1,4 +1,4 @@
-import { useState, useTransition, useDeferredValue} from "react";
+import { useState, useTransition, useDeferredValue, useEffect} from "react";
 
 
 const Testpage = 
@@ -9,21 +9,37 @@ function(){
     let [isPending, startTransition] = useTransition()
     let state = useDeferredValue(name)
 
+    // async(동기 / 비동기 처리 방식) 예시
+    let [count, setcount] = useState(0)
+    let [age, setage] = useState(20)
+    useEffect(()=>{
+        if(count !== 0 && count < 3){
+            setage(age + 1);
+        }
+    },[count]);
     return(
         <div>
-            {name}
-            <input onChange={(e)=>{ 
-                startTransition(()=>{
-                    setname(e.target.value)  
-                })
-            }}></input>
             <div>
-                {
-                    isPending ? '로딩중' :
-                    arr.map((a, i)=>{
-                        return <div key={i}>{state}</div>
+                {name}
+                <input onChange={(e)=>{ 
+                    startTransition(()=>{
+                        setname(e.target.value)  
                     })
-                }
+                }}></input>
+                <div>
+                    {
+                        isPending ? '로딩중' :
+                        arr.map((a, i)=>{
+                            return <div key={i}>{state}</div>
+                        })
+                    }
+                </div>
+            </div>
+            <div>
+                <div>안녕하십니까 전 {age}</div>
+                <button onClick={()=>{
+                    setcount(count + 1);
+                }}>누르면한살먹기</button>
             </div>
         </div>
     )
